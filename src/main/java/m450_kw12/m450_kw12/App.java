7e201716825ba3b33a2f5cef9626ad0afae0f14a
tsolinger <119655594+tsolinger@users.noreply.github.com>
2026-03-20 09:20:08 +0100

    initial Maven project with tests

---
+++ src/main/java/m450_kw12/m450_kw12/App.java
@@ -0,0 +1,13 @@
+package m450_kw12.m450_kw12;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/m450_kw12/m450_kw12/SampleTest.java
@@ -0,0 +1,14 @@
+package m450_kw12.m450_kw12;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+
+	@Test
+	void firstTestRun() {
+		assertEquals(3, 3);
+	}
+
+}
